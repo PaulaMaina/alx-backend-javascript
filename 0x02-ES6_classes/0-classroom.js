846b942abfc4b6e187ff2171efb64caa5b96ad4a
PaulaMaina <kabura.mainap@gmail.com>
2024-07-25 15:17:32 +0300

    Task 0

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,4 +1,12 @@
+/**
+Declares a new class called ClassRoom
+*/
 export default class ClassRoom {
+  /**
+   * Creates a new @see {@link ClassRoom}.
+   * @param {Number} maxStudentsSize - The maximum number of
+   * students in the class
+   */
     constructor(maxStudentsSize) {
     this._maxStudentsSize = maxStudentsSize;
   };
