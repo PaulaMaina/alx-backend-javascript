b6f6364a099ac568e108b4fbf68e5d684289654a
PaulaMaina <kabura.mainap@gmail.com>
2024-07-25 15:36:14 +0300

    Task 1 debug

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,8 +1,5 @@
 import ClassRoom from "./0-classroom";
-/**
- * Implements the initializeRooms function.
- * @returns an array of {@link ClassRoom}s.
- */
+
 export default function initializeRooms() {
     return [19, 20, 34].map((size) => new ClassRoom(size));
 }
