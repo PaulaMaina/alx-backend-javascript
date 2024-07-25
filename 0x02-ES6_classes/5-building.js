77a78dfbe3bf82b2d36ffa9bee01fe5a679a3f3d
PaulaMaina <kabura.mainap@gmail.com>
2024-07-25 16:54:31 +0000

    A Building

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,23 @@
+export default class Building {
+  constructor(sqft) {
+    this._sqft = sqft;
+    if (this.constructor !== Building) {
+      if (typeof this.evacuationWarningMessage !== 'function') {
+        throw new Error(
+          'Class extending Building must override evacuationWarningMessage'
+	);
+      }
+    }
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('SQFT must be a number');
+    }
+    this._sqft = value;
+  }
+}
