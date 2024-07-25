598241214d748ed00952904801e608153488a8e6
PaulaMaina <kabura.mainap@gmail.com>
2024-07-25 14:08:48 +0000

    Task 2

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -5,37 +5,39 @@ export default class HolbertonCourse {
     this._students = students;
   }
 
-  set name(name) {
-    if (typeof name !== 'string') {
-      throw new TypeError('Name must be a string');
-    }
-    this._name = name;
-  }
-
   get name() {
     return this._name;
   }
 
-  set length(length) {
-    if (typeof length !== 'number') {
-      throw new TypeError('Length must be a number');
+  set name(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('Name must be a string');
     }
-    this._length = length;
+    this._name = value;
   }
 
   get length() {
     return this._length;
   }
 
-  set students(students) {
-    if (students instanceof Array) {
-      this._students = students;
-    } else {
-      throw new TypeError('Students should be an array');
+  set length(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('Length must be a number');
     }
+    this._length = value;
   }
 
   get students() {
     return this._students;
+  }
+
+  set students(value) {
+    if (!(value instanceof Array)) {
+      throw new TypeError('Students must be an array');
+    }
+    if (!value.every((student) => typeof student === 'string')) {
+      throw new TypeError('Students should be an array of strings');
+    }
+    this._students = value;
   }
 }
