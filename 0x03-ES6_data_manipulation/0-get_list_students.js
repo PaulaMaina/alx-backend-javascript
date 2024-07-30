9fde196addca918b128ed082e50acad521cad244
PaulaMaina <kabura.mainap@gmail.com>
2024-07-30 15:17:44 +0000

    Basic list of objects

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,4 +1,4 @@
-export default function getListStudenst() {
+export default function getListStudents() {
   return [
     { id: 1, firstname: 'Guillaume', location: 'San Francisco' },
     { id: 2, firstName: 'James', location: 'Columbia' },
