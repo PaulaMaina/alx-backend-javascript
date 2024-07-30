8d7400c031fad140c42e8b0b2bdf1cc727a9b0d8
PaulaMaina <kabura.mainap@gmail.com>
2024-07-30 16:01:45 +0000

    Groceries list

--- 0x03-ES6_data_manipulation/9-groceries_list.js
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -1,11 +1,11 @@
 export default function groceriesList() {
-  const groceries = new Map(
+  const groceries = [
     ['Apples', 10],
     ['Tomatoes', 10],
     ['Pasta', 1],
     ['Rice', 1],
     ['Banana', 5],
-  );
+  ];
 
-  return groceries;
+  return new Map(groceries);
 }
