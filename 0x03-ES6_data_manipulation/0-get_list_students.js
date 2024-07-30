2069e345f0f2ded4bd99ec11086587b882380737
PaulaMaina <kabura.mainap@gmail.com>
2024-07-30 12:46:15 +0000

    First commit

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,7 @@
+export default function getListStudent(id, firstName, location) {
+  return [
+    { id: 1, firstname: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  ];
+}
