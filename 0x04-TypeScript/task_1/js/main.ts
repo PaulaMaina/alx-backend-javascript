f3781645dd4a22e64abe06ba941891fcc01082be
PaulaMaina <kabura.mainap@gmail.com>
2024-07-31 14:21:23 +0000

    TypeScript

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -10,3 +10,38 @@ export interface Teacher {
 export interface Director extends Teacher {
   numberOfReport: number;
 }
+
+export interface printTeacherFunction {
+  (firstName: string, lastName:string): string;
+}
+
+export function printTeacher(firstName: string, lastName:string): string {
+  return `${firstName[0]}. ${lastName}`;
+}
+
+export interface InStudentClassConstructor {
+  new (firstName: string, lastName: string): InStudentClass;
+}
+
+export interface InStudentClass {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+export class StudentClass implements InStudentClass {
+  private _firstName!: string;
+  private _lastName!: string;
+
+  constructor(firstName: string, lastName: string) {
+    this._firstName = firstName;
+    this._lastName = lastName;
+  }
+
+  workOnHomework() {
+    return 'Currently working';
+  }
+
+  displayName () {
+    return this._firstName;
+  }
+}
