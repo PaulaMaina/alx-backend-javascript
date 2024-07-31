60883c3fe3bef596fd8cc660230bd213985aa018
PaulaMaina <kabura.mainap@gmail.com>
2024-07-31 13:23:59 +0000

    First commit

---
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -0,0 +1,52 @@
+interface Student {
+  firstName: string;
+  lastName: string;
+  age: number;
+  location: string;
+}
+
+const student1: Student = {
+  firstName: "Alexis";
+  lastName: "Maina";
+  age: 30;
+  location: "United Kingdom";
+};
+
+const student2: Student = {
+  firstName: "Jayden";
+  lastName: "Otieno";
+  age: 25;
+  location: "United States";
+};
+
+const studentsList: Array<Student> = [
+  student1,
+  student2,
+];
+
+export const displayStudents = (students: Array<Student>): void => {
+  const table = document.createElement('table');
+  const tableHead = document.createElement('thead');
+  const headerRow = document.createElement('tr');
+  const tableBody = document.createElement('tbody');
+  headerRow.insertAdjacentHTML('beforeend', '<td>FirstName</td');
+  headerRow.insertAdjacentHTML('beforeend', '<td>Location</td');
+  tableHead.insertAdjacentElement('beforeend', headerRow);
+
+  for (const student of students) {
+    const bodyRow = document.createElement('tr');
+    bodyRow.insertAdjacentHTML('beforeend', `<td>${student.firstName}</td>`);
+    bodyRow.insertAdjacentHTML('beforeend', `<td>${student.location}</td>`);
+    tableBody.insertAdjacentElement('beforeend', bodyRow);
+  }
+
+  table.insertAdjacentElement('beforeend', tableHead);
+  table.insertAdjacentElement('beforeend', tableBody);
+  document.body.insertAdjacentElement('beforeend', table);
+};
+
+displayStudents(studentsList);
+const styleSheetElement = document.createElement('style');
+styleSheetElement.innerHTML = styleSheet;
+document.head.insertAdjacentElement('beforeend', styleSheetElement);
+document.title = 'Task 0';
