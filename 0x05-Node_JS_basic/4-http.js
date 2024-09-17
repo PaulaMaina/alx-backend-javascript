73169b91cdb9fe8b95edf3347507c07efafea09e
PaulaMaina <kabura.mainap@gmail.com>
2024-09-17 13:29:27 +0000

    Basic Node server

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,19 +1,18 @@
 const http = require('http');
 
-const port = 1245;
-const host = 'localhost';
 const app = http.createServer();
 
 app.on('request', (_, res) => {
   const resText = "Hello Holberton School!";
+
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.setHeader('Content-Length', resText.length);
   res.write(Buffer.from(resText));
 });
 
-app.listen(port, host, () => {
-  process.stdout.write(`Server running at http://${host}:${port}\n`);
+app.listen(1245, 'localhost', () => {
+  process.stdout.write('Server running at http://localhost:1245\n');
 });
 
 module.exports = app;
