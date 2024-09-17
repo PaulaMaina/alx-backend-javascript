b7e652f464ab73fe058c7cd49c366c84ab5e2d5e
PaulaMaina <kabura.mainap@gmail.com>
2024-09-17 11:36:55 +0000

    Process stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,12 +1,10 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
-
 process.stdin.on('readable', () => {
-  let username = process.stdin.read();
+  const username = process.stdin.read();
   if (username) {
     process.stdout.write(`Your name is: ${username}`);
   }
 });
-
 process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing\n');
 });
